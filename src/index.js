3a51073891bb8cc5a243d9b5958b3bb74a0e75ac
maxhaponenko <gaponenko.mm@gmail.com>
2019-08-09 17:29:44 +0300

    deleted materialize

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './js/common';
+import './js/navigation';
+// import 'materialize-css/dist/js/materialize.js';
+
+import 'bootstrap';
+// import 'materialize-css/dist/css/materialize.css';
+
+
+import './scss/main.scss';
+
+import './css/main.css';
+
