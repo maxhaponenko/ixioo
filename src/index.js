51a8c6cece9f0ce8c8a2b98347aff778b6093743
max.haponenko <38444277+maxhaponenko@users.noreply.github.com>
2019-08-24 01:05:14 +0300

    career page modified

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import './js/common';
 import './js/navigation';
-// import 'materialize-css/dist/js/materialize.js';
 import './js/video-settings';
 // import './js/nav-color-animation';
 
 import 'bootstrap';
-// import 'materialize-css/dist/css/materialize.css';
-
 
 import './scss/main.scss';
-
 import './css/main.css';
 
